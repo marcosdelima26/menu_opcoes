f896ae447f45729e87f74e9780ffdc323106e755
Marcos Lima <94881434+marcosdelima26@users.noreply.github.com>
2022-10-23 18:42:25 -0300

    Add files via upload

---
+++ resolucao_criando_menu_opcoes.py
@@ -0,0 +1,38 @@
+from time import sleep
+n1 = int(input('Digite o primeiro valor: '))
+n2 = int(input('Digite o segundo valor: '))
+opcao = 0
+
+while opcao != 5:
+    print('''    [ 1 ] Somar
+    [ 2 ] Multiplicar
+    [ 3 ] Maior
+    [ 4 ] Novos números
+    [ 5 ] Sair do programa
+    ''')
+
+    opcao = int(input('Qual é a sua opção? '))
+
+    if opcao == 1:
+        soma = n1 + n2
+        print('A soma entre {} e {} é {}.'.format(n1, n2, soma))
+    elif opcao == 2:
+        produto = n1 * n2
+        print('O resultado de {} X {} é {}.'.format(n1, n2, produto))
+    elif opcao == 3:
+        if n1 > n2:
+            maior = n1
+        else:
+            maior = n2
+        print('Entre {} e {} o maior valor é {}.'.format(n1, n2, maior))
+    elif opcao == 4:
+        print('Informe os números novamente: ')
+        n1 = int(input('Primeiro valor: '))
+        n2 = int(input('Segundo valor: '))
+    elif opcao == 5:
+        print('Finalizando...')
+    else:
+        print('Opção inválida. Tente novamente!')
+    print('=-=' * 10)
+    sleep(2)
+print('Fim do programa! Volte Sempre!')
